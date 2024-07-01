c1ca0377add713aa7157e1157f6fc74503441d5d
anhye0n <anhye0ndev@gmail.com>
2024-07-01 18:32:31 +0900

    chore: initial project setup

---
+++ src/store/store.ts
@@ -0,0 +1,26 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import { persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+
+import userSlice, { UserInfo } from "@store/userSlice.ts"
+
+export interface RootState {
+    userInfo: UserInfo
+}
+
+const reducers = combineReducers({
+    userInfo: userSlice.reducer
+})
+
+const persistConfig = {
+    key: "root", // localStorage key
+    storage, // localStorage
+    whitelist: ["userInfo"] // target (reducer name)
+}
+
+const persistStore = persistReducer(persistConfig, reducers)
+
+export default configureStore({
+    reducer: persistStore,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@src", replacement: resolve(__dirname, "./src")
+      },
+      {
+        find: "@components", replacement: resolve(__dirname, "./src/components")
+      },
+      {
+        find: "@pages", replacement: resolve(__dirname, "./src/pages")
+      },
+      {
+        find: "@assets", replacement: resolve(__dirname, "./src/assets")
+      },
+      {
+        find: "@store", replacement: resolve(__dirname, "./src/store")
+      },
+      {
+        find: "@plugins", replacement: resolve(__dirname, "./src/plugins")
+      }
+    ]
+  }
 })
