7f5396ac346829fd1e9e1be29adeafe4b80ea154
문지원 <81554184+jeewonMoon@users.noreply.github.com>
2024-07-06 12:09:13 +0900

    3 design 글로벌 스타일 정의 (#4)
    * design: 글로벌 스타일 정의
    
    - styled-components 설치
    - 기본 css 스타일 초기화
    - 텍스트와 색상 테마 정의
    
    * design: 색상 테마 수정
    
    - 2회 이상 사용되는 색상들만 남겼습니다.

--- src/pages/index/Home.tsx
+++ src/pages/index/Home.tsx
@@ -1,9 +1,15 @@
+import styled from 'styled-components';
+
 function Home() {
     return (
-        <div>
-            Home
-        </div>
+        <Container>
+            강의시간표/수업계획서조회
+        </Container>
     );
 }
+
+const Container = styled.div`
+    ${props=>props.theme.texts.title};
+`;
 
 export default Home;
---
+++ src/styles/theme/Theme.ts
@@ -0,0 +1,60 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+  primary: '#a31432',
+  secondary: '#334D61',
+  neutral1: '#222',
+  neutral2: '#333',
+  neutral3: '#444',
+  neutral4: '#E8E8E8',
+  neutral5: '#F7F7F7',
+  blue: '#A9D0F5',
+  black: '#000',
+  white: '#FFF',
+};
+
+const texts = {
+    title: {
+        fontSize: '2.1rem',
+        fontWeight: '700',
+        color: '#222'
+    },
+    subtitle: {
+        fontSize: '1.4rem',
+        fontWeight: '600',
+        color: '#333'
+    },
+    content: {
+        fontSize: '1.2rem',
+        fontWeight: '400',
+        color: '#444'
+    },
+    warning: {
+        fontSize: '1.2rem',
+        fontWeight: '600',
+        color: '#FAEBEE'
+    },
+    tapTitle: {
+        fontSize: '1.3rem',
+        fontWeight: '400',
+        color: '#444'
+    },
+    tapTitleFocus: {
+        fontSize: '1.3rem',
+        fontWeight: '600',
+        color: '#a31432'
+    },
+    tableTitle: {
+        fontSize: '1.2rem',
+        fontWeight: '600',
+        color: '#444'
+    },
+};
+
+export type ColorsType = typeof colors;
+export type TextsType = typeof texts;
+
+export const theme: DefaultTheme = {
+    colors,
+    texts
+};
