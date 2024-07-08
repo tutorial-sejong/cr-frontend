9417b312324e03be0fb4706d6387fb318a29d0d0
문지원 <81554184+jeewonMoon@users.noreply.github.com>
2024-07-09 00:25:15 +0900

    feat: 로그인 페이지 퍼블리싱 (#7)
    - document title을 임의로 수강신청으로 변경했습니다.
    - assets/img 폴더에 필요한 이미지 파일들을 넣어놨습니다.
    - 로그인 폼의 인풋을 컴포넌트화했습니다.
    - 로그인 화면에서 겹치는 텍스트 스타일을 테마에 추가했습니다.

---
+++ src/components/LoginForm/index.tsx
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import FormInput from './FormInput';
+import {useState} from 'react';
+
+export type setType = string | number;
+
+function LoginForm() {
+  const [id, setId] = useState<setType>(0);
+  const [name, setName] = useState<setType>('');
+
+  return (
+    <FormContainer>
+      <InputContainer>
+        <InputBox>
+          <LabelWrap>학번</LabelWrap>
+          <FormInput value={id} setValue={setId} type='number' />
+        </InputBox>
+        <InputBox>
+          <LabelWrap>이름</LabelWrap>
+          <FormInput value={name} setValue={setName} type='text' />
+        </InputBox>
+        <CheckboxWrap>
+          <input type='checkbox' id='keyboardSecurity' checked readOnly />
+          <label htmlFor='keyboardSecurity'>키보드 보안</label>
+        </CheckboxWrap>
+      </InputContainer>
+      <FindWrap>아이디 찾기 | 비밀번호 찾기</FindWrap>
+      <LoginBtnWrap>로그인</LoginBtnWrap>
+    </FormContainer>
+  );
+}
+
+const FormContainer = styled.div`
+  padding: 1.5rem 4rem;
+  background-color: ${props => props.theme.colors.white};
+  border-radius: 0.3rem;
+  margin-bottom: 2rem;
+`;
+
+const InputContainer = styled.div`
+  margin-bottom: 2.5rem;
+`;
+
+const InputBox = styled.div`
+  margin-top: 2rem;
+`;
+
+const LabelWrap = styled.div`
+  ${props => props.theme.texts.loginContent};
+  margin-bottom: 0.7rem;
+`;
+
+const CheckboxWrap = styled.div`
+  margin-left: -0.5rem;
+  margin-top: 1rem;
+`;
+
+const FindWrap = styled.div`
+  ${props => props.theme.texts.tableTitle};
+  color: #777;
+  float: inline-end;
+  margin-bottom: 2.5rem;
+`;
+
+const LoginBtnWrap = styled.button`
+  width: 100%;
+  height: 5rem;
+  background-color: #c3002f;
+  border: none;
+  border-radius: 5rem;
+  color: ${props => props.theme.colors.white};
+  font-size: 1.7rem;
+  font-weight: 700;
+  box-shadow: 0px 4px 5px lightgray;
+  margin-bottom: 2rem;
+
+  &:hover {
+    background-color: #c3002fc7;
+  }
+`;
+
+export default LoginForm;
