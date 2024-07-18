accc833aab94d6c67be22e2bab56ac069bbe68f7
문지원 <81554184+jeewonMoon@users.noreply.github.com>
2024-07-18 15:17:43 +0900

    feat: 상단바 구현 (#17)
    - neutral4: #777을 추가했습니다.
    - 기존의 neutral4와 neutral5는 neutral5, 6로 순번이 밀렸습니다.
    - 이에 따른 수정 사항들을 수정했습니다.
    - 학번 칸에 0이 기본적으로 들어가 있는 버그를 수정했습니다.
    - input 디자인도 수정했습니다.
    - 필요한 이미지 파일들을 추가했습니다.
    - 로그인 유효시간 타이머 기능을 추가했습니다.
    현재는 다른 처리 없이 로그인 화면으로 이동시키고 있습니다.
    - 로그아웃 버튼도 동일하게 로그인 화면으로 이동하도록 했습니다.

---
+++ src/components/Header.tsx/TopMenu.tsx
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+import {useNavigate} from 'react-router-dom';
+import Left from '@assets/img/btn_main_top_left.svg?react';
+import Right from '@assets/img/btn_main_top_right.svg?react';
+import logout from '@assets/img/logout.png';
+import down from '@assets/img/top_menu_down.png';
+import notice from '@assets/img/notice.png';
+import setting from '@assets/img/setitng.png';
+import menu from '@assets/img/menu.png';
+import Timer from './TImer';
+
+function TopMenu() {
+  const navigate = useNavigate();
+  const name = '세종대';
+
+  const handleLogout = () => {
+    navigate('/login');
+  };
+
+  return (
+    <TopMenuContainer>
+      <ArrowWrap>
+        <StyledLeft />
+        <StyledRight />
+      </ArrowWrap>
+      <Timer name={name} />
+      <LogoutBtn onClick={handleLogout} />
+      <DropdownWrap>
+        PC
+        <img src={down} />
+      </DropdownWrap>
+      <DropdownWrap>
+        KOR
+        <img src={down} />
+      </DropdownWrap>
+      <GroupWrap>
+        <img src={notice} />
+        <img src={setting} />
+        <img src={menu} />
+      </GroupWrap>
+    </TopMenuContainer>
+  );
+}
+
+const TopMenuContainer = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 1rem;
+`;
+
+const ArrowWrap = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 1.2rem;
+  margin-right: 1.5rem;
+`;
+
+const StyledLeft = styled(Left)`
+  fill: ${props => props.theme.colors.neutral4};
+  &:hover {
+    fill: ${props => props.theme.colors.primary};
+  }
+`;
+
+const StyledRight = styled(Right)`
+  fill: ${props => props.theme.colors.neutral4};
+  &:hover {
+    fill: ${props => props.theme.colors.primary};
+  }
+`;
+
+const LogoutBtn = styled.button`
+  background-image: url(${logout});
+  width: 1.4rem;
+  height: 1.4rem;
+
+  &:hover {
+    filter: brightness(20%);
+  }
+`;
+
+const DropdownWrap = styled.div`
+  ${props => props.theme.texts.tableTitle};
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  column-gap: 1.5rem;
+
+  > img {
+    &:hover {
+      filter: brightness(20%);
+    }
+  }
+`;
+
+const GroupWrap = styled.div`
+  display: flex;
+  column-gap: 1rem;
+
+  > img {
+    &:hover {
+      filter: brightness(20%);
+    }
+  }
+`;
+
+export default TopMenu;
