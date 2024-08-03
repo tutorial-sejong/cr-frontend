ab0bb4740dd30711013365f34c0c869e601c8779
jeewonMoon <mjw9912@gmail.com>
2024-08-03 20:24:09 +0900

    fix: 타입에러 등 오류 수정
    - 빌드 console.log 테스트하는 과정에서 오류를 수정했습니다.

--- src/components/CourseRegister/StartButton.tsx
+++ src/components/CourseRegister/StartButton.tsx
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 import {openModalHandler} from '@components/common/Modal/handlers/handler.tsx';
-import {getCourseList, getWishlist} from '@apis/api/course.ts';
-import {FiltersProps} from '@components/LectureList/Filters.tsx';
+import {getWishlist} from '@apis/api/course.ts';
 import {useAppSelector} from '@store/hooks';
 import {useDispatch} from 'react-redux';
+import {CourseTypes} from '@/assets/types/tableType';
 
-function StartButton({setList, setStartVisible}: {setList: FiltersProps}) {
+interface StartBtnProps {
+  setList: React.Dispatch<React.SetStateAction<CourseTypes[]>>;
+  setStartVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
+function StartButton({setList, setStartVisible}: StartBtnProps) {
   const studentId = useAppSelector(state => state.userInfo.username);
 
   const dispatch = useDispatch();
@@ -34,22 +38,22 @@ function StartButton({setList, setStartVisible}: {setList: FiltersProps}) {
 }
 
 const Container = styled.div`
-    > p {
-        font-weight: normal;
-        font-size: 1.6rem;
-        margin-bottom: 5px;
-    }
+  > p {
+    font-weight: normal;
+    font-size: 1.6rem;
+    margin-bottom: 5px;
+  }
 `;
 const ButtonWrap = styled.button`
-    ${props => props.theme.texts.content};
-    background-color: ${props => props.theme.colors.primary};
-    color: ${props => props.theme.colors.white};
-    width: 6rem;
-    height: 2.4rem;
-    margin-bottom: 10px;
+  ${props => props.theme.texts.content};
+  background-color: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.white};
+  width: 6rem;
+  height: 2.4rem;
+  margin-bottom: 10px;
 
-    &:hover {
-        filter: grayscale(15%);
-    }
+  &:hover {
+    filter: grayscale(15%);
+  }
 `;
 export default StartButton;
